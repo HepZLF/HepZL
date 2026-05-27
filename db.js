884b6c6bea6141a017d8668ddc792b351e0d2309
HepZLF <hepryzallhosasiii@gmail.com>
2026-05-27 19:42:28 +0700

    fix: menyelaraskan URL Supabase di db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 // db.js
 
-// Konfigurasi URL dan API Key Supabase Anda
-const supabaseUrl = 'https://igrhhxyxjiqharbgyfau.supabase.co';
+// Konfigurasi URL dan API Key Supabase yang Sesuai dengan .env Anda
+const supabaseUrl = 'https://safbzgsfjwuuosuvzgbe.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlncmhoeHl4amlxaGFyYmd5ZmF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4MDA3NTMsImV4cCI6MjA5NTM3Njc1M30.LfKfEtnPwQ37OCLc7xzrP5cb-iI6nseJMYSy2l59Li4';
 
 // Membuat client Supabase dan menyimpannya ke objek window agar bisa diakses global oleh script lain
